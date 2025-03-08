eb161b45c125ccbc9ce0d7dbff26ff2c1d7bb268
Matty <mattyjh90@hotmail.co.uk>
2025-03-08 15:22:23 +0000

    Added failing test for the scan with single item B scenario, also renamed some test names to be more explicit

--- Checkout.Tests/CheckoutTests.cs
+++ Checkout.Tests/CheckoutTests.cs
@@ -18,7 +18,7 @@ public class CheckoutTests
     }
 
     [Test]
-    public void Scan_WithASingleItem_AddsItemToTheCheckout()
+    public void Scan_WithASingleItemA_AddsItemToTheCheckout()
     {
         var checkout = new Checkout();
 
@@ -26,5 +26,16 @@ public class CheckoutTests
 
         var total = checkout.GetTotalPrice();
         Assert.That(total, Is.EqualTo(50m));
+    }
+
+    [Test]
+    public void Scan_WithASingleItemB_AddsItemToTheCheckout()
+    {
+        var checkout = new Checkout();
+
+        checkout.Scan("B");
+
+        var total = checkout.GetTotalPrice();
+        Assert.That(total, Is.EqualTo(30m));
     }
 }
