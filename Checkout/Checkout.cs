7bfd3ca0627de35d59eb184d59a5737a621336ee
Matty <mattyjh90@hotmail.co.uk>
2025-03-08 17:06:55 +0000

    Did a cleanup of the get total price algorithm, including renaming scannedItems property to be more explicit. Also extracted Item classes into their own file

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -1,39 +1,9 @@
 ﻿namespace Checkout;
 
-public interface Item
-{
-    public string Sku { get; }
-    public decimal Price { get; }
-}
-
-public class ItemA : Item
-{
-    public string Sku { get { return "A"; } }
-    public decimal Price { get { return 50m; } }
-}
-
-public class ItemB : Item
-{
-    public string Sku { get { return "B"; } }
-    public decimal Price { get { return 30m; } }
-}
-
-public class ItemC : Item
-{
-    public string Sku { get { return "C"; } }
-    public decimal Price { get { return 20m; } }
-}
-
-public class ItemD : Item
-{
-    public string Sku { get { return "D"; } }
-    public decimal Price { get { return 15m; } }
-}
-
 public class Checkout
 {
     private readonly IEnumerable<Item> _availableItems = new List<Item>();
-    private readonly IList<string> _scannedItems = new List<string>();
+    private readonly IList<string> _scannedSkus = new List<string>();
 
     public Checkout(IEnumerable<Item> availableItems)
     {
@@ -42,23 +12,19 @@ public class Checkout
 
     public void Scan(string sku)
     {
-        _scannedItems.Add(sku);
+        _scannedSkus.Add(sku);
     }
 
     public decimal GetTotalPrice()
     {
         var totalPrice = 0m;
 
-        if (_scannedItems.Any())
+        foreach (var sku in _scannedSkus)
         {
-            foreach (var sku in _scannedItems)
-            {
-                var scannedItem = _availableItems.First((i) => i.Sku == sku);
-                totalPrice += scannedItem.Price;
-            }
-            return totalPrice;
+            var scannedItem = _availableItems.First((i) => i.Sku == sku);
+            totalPrice += scannedItem.Price;
         }
 
-        return 0;
+        return totalPrice;
     }
 }
