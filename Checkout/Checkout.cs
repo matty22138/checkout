3dde98590d07dc21e9ccce0354a04bdb49277845
Matty <mattyjh90@hotmail.co.uk>
2025-03-08 15:20:33 +0000

    Got test passing for the scan with single item scenario, by faking the return price of item A

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -2,13 +2,20 @@
 
 public class Checkout
 {
+    private readonly List<string> _scannedItems = new List<string>();
+
     public void Scan(string sku)
     {
-
+        _scannedItems.Add(sku);
     }
 
     public decimal GetTotalPrice()
     {
+        if (_scannedItems.Any())
+        {
+            return 50m;
+        }
+
         return 0;
     }
 }
